d9a9f095225721963d30828c5c224b0799354786
Johny-Silverhand <145774894+Johny-Silverhand@users.noreply.github.com>
2026-09-05 12:25:29 -0700

    fix(v1.2.2): MessageBoxW for launch errors

--- sys_windows.go
+++ sys_windows.go
@@ -2,7 +2,10 @@
 
 package main
 
-import "syscall"
+import (
+	"syscall"
+	"unsafe"
+)
 
 func hideWindow() *syscall.SysProcAttr {
 	return &syscall.SysProcAttr{HideWindow: true}
@@ -10,4 +13,21 @@ func hideWindow() *syscall.SysProcAttr {
 
 func showWindow() *syscall.SysProcAttr {
 	return &syscall.SysProcAttr{HideWindow: false}
+}
+
+func showError(title, msg string) {
+	user32 := syscall.NewLazyDLL("user32.dll")
+	messageBoxW := user32.NewProc("MessageBoxW")
+	t, _ := syscall.UTF16PtrFromString(title)
+	m, _ := syscall.UTF16PtrFromString(msg)
+	// MB_OK | MB_ICONERROR | MB_SETFOREGROUND | MB_TOPMOST
+	messageBoxW.Call(0, uintptr(unsafe.Pointer(m)), uintptr(unsafe.Pointer(t)), 0x00000010|0x00010000|0x00040000)
+}
+
+func showInfo(title, msg string) {
+	user32 := syscall.NewLazyDLL("user32.dll")
+	messageBoxW := user32.NewProc("MessageBoxW")
+	t, _ := syscall.UTF16PtrFromString(title)
+	m, _ := syscall.UTF16PtrFromString(msg)
+	messageBoxW.Call(0, uintptr(unsafe.Pointer(m)), uintptr(unsafe.Pointer(t)), 0x00000040|0x00010000)
 }
